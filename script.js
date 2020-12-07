4bee8a9a9e6e7c225e2a84e9d0ed02887f1697e4
Ashutosh Chauhan <ashutoshchauhan326@gmail.com>
2020-12-07 18:39:16 +0530

    modified script for postgres timings

--- script.js
+++ script.js
@@ -22,27 +22,27 @@ createDbEntries = async () => {
         ltp float
     )`);
 
-    await client.query(`INSERT INTO portfolio VALUES (1, 'ASHOKA', 35, 64.23, 86.55),
-	(1, 'AXISBANK', 3, 433, 614.5),
-	(1, 'BANDHANBNK', 10, 280, 393.4),
-	(1, 'BANKBARODA', 100, 41.8, 59.05),
-	(1, 'BHARTIARTL', 15, 482, 493.90),
-	(1, 'BRITANIA', 2, 3660, 3648),
-	(1, 'BSOFT', 5, 104, 188.5),
-	(1, 'DEEPAKNTR', 3, 762, 845.05),
-	(1, 'FEDERALBNK', 250, 53.10, 66),
-	(1, 'GAIL', 30, 90.16, 119.8),
-	(1, 'HDFC', 1, 1586.50, 2245.9),
-	(1, 'HDFCBANK', 3, 888.67, 1385.6),
-	(1, 'HDFCLIFE', 15, 595.53, 643.25),
-	(1, 'HINDPETRO*', 10, 211.80, 216.6),
-	(1, 'HINDUNILVR', 3, 2049.63, 2184.2),
-	(1, 'ICICIBANK', 30, 401.16, 502.15),
-	(1, 'INDUSINDBK', 12, 612, 913.65),
-	(1, 'INFY', 30, 1080.66, 1134.65),
-	(1, 'ITC', 30, 168.93, 198.15),
-	(1, 'NETWORK18', 28, 44.65, 36.70),
-    (1, 'YESBANK', 1000, 12, 15.33)`);
+    await client.query(`INSERT INTO portfolio VALUES (1, 'ASHOKA', 35, 64.23, 100),
+	(1, 'AXISBANK', 3, 433, 100),
+	(1, 'BANDHANBNK', 10, 280, 100),
+	(1, 'BANKBARODA', 100, 41.8, 100),
+	(1, 'BHARTIARTL', 15, 482, 100),
+	(1, 'BRITANIA', 2, 3660, 100),
+	(1, 'BSOFT', 5, 104, 100),
+	(1, 'DEEPAKNTR', 3, 762, 100),
+	(1, 'FEDERALBNK', 250, 53.10, 100),
+	(1, 'GAIL', 30, 90.16, 100),
+	(1, 'HDFC', 1, 1586.50, 100),
+	(1, 'HDFCBANK', 3, 888.67, 100),
+	(1, 'HDFCLIFE', 15, 595.53, 100),
+	(1, 'HINDPETRO*', 10, 211.80, 100),
+	(1, 'HINDUNILVR', 3, 2049.63, 100),
+	(1, 'ICICIBANK', 30, 401.16, 100),
+	(1, 'INDUSINDBK', 12, 612, 100),
+	(1, 'INFY', 30, 1080.66, 100),
+	(1, 'ITC', 30, 168.93, 100),
+	(1, 'NETWORK18', 28, 44.65, 100),
+    (1, 'YESBANK', 1000, 12, 100)`);
 
     await client.query(
       `CREATE TABLE instruments(user_id int, name varchar(20), ltp float)`
@@ -51,8 +51,8 @@ createDbEntries = async () => {
       `insert into instruments values(1, 'ASHOKA', 100), (1, 'GAIL', 100)`
     );
 
-    await client.query(`CREATE TABLE ORDERS(id serial, user_id int, type varchar(10), instrument varchar(20), quantity int, price float, status varchar(20), time varchar(30))
-    `);
+    await client.query(`CREATE TABLE ORDERS(id serial, user_id int, type varchar(10), instrument varchar(20), quantity int, price float, status varchar(20), date DATE DEFAULT CURRENT_DATE, time TIME DEFAULT CURRENT_TIME(0)
+    )`);
 
     await client.end();
   } catch (err) {
